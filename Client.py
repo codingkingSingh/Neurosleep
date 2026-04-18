776b82ed5f2e039737673d052a7ec04f9652b189
loledric <82963707+loledric@users.noreply.github.com>
2026-04-18 02:50:17 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,3 +1,12 @@
 import socket
 
-Host 
+HOST = '127.0.0.1'
+PORT = 8000
+
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+
+    client_socket.connect((HOST, PORT))
+    print ("Connected to server at {HOST}:{PORT}")
+
+
+
