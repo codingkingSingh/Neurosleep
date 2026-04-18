fd88b70c38ff646490c63bfdd65ddbe3e5149490
Manny Singh <msingh2@ualr.edu>
2026-04-18 17:40:48 +0000

    Finished Issue 1:

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ Neurosleep_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM )
 
 Port = 8000
 
+LocalHost = '127.0.0.1'
+
 # I chose port 8000, please check this 
 
 
-Neurosleep_socket.bind('', Port)
+
+Neurosleep_socket.bind((LocalHost, Port))
 print("Server is open....")
+
+Neurosleep_socket.listen()
+
+connection, client_address = Neurosleep_socket.accept()
+
+data = connection.recv(1024) 
+print(data.decode())
