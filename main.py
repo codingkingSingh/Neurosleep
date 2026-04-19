0d78f3283f21e07fadfee6e9031011517c607128
loledric <82963707+loledric@users.noreply.github.com>
2026-04-19 18:31:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import socket 
+import socket
+import datetime as dt
+import cache
 
 Neurosleep_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM )
 
@@ -21,5 +23,13 @@ while True:
 
     data = connection.recv(1024) 
     print(data.decode())
+
+    # Storing incoming message into Cache.py
+    Cache.Temp_Cache[client_address] = {
+        "Message": data.encode(),
+        "Timestamp': dt.datetime.now()
+    }
+
+    print(cache.Temp_Cache) # for testing 
 
     connection.close()
