8a6f2478fa589e49470731994380da3ffd1ea175
loledric <82963707+loledric@users.noreply.github.com>
2026-04-19 20:17:18 -0500

    added sleep thread & mutex lock

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import socket
 import datetime as dt
 import Cache
+import threading
+import time
+
+Cache_lock = threading.Lock()
+
 
 Neurosleep_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM )
 
@@ -10,12 +15,48 @@ LocalHost = '127.0.0.1'
 
 # I chose port 8000, please check this 
 
-
-
 Neurosleep_socket.bind((LocalHost, Port))
 print("Server is open....")
 
 Neurosleep_socket.listen()
+
+
+def sleeping_thread():
+    while True:
+        # temporary timer, please change as needed, wakes up every 10 secs
+        time.sleep(10) 
+
+        print("Wake up to clean cache")
+
+        Cache_lock.acquire()
+
+        try:
+            time_now = dt.datetime.now()
+
+            expired_message= []
+
+            # find expired/old messages, currently setting it up at 30 secs
+            for Client, date in Cache.Temp_Cache.items():
+                age = (time_now - date["Timestamp"]).seconds
+                if age > 30: # please change this as needed
+                    expired_message.append(Client)
+
+
+            for Client in expired_message:
+                print(f"{Client} - message deleted")
+                del Cache.Temp_Cache[Client]
+
+
+        finally:
+            print ("Going back to sleep")
+            Cache_lock.release()   
+
+    
+
+    
+cleaner = threading.Thread(target=sleeping_thread)
+cleaner.daemon = True
+cleaner.start()
 
 while True: 
 
